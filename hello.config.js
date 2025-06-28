c7ea66fee43258bcdac765309841941a40822fb7
kuukitenshi <lauracunha803@outlook.pt>
2025-06-28 15:44:13 +0100

    quickstart

--- hello.config.js
+++ hello.config.js
@@ -5,3 +5,5 @@ const config = {
 //    provider_hint:[]
 }
 module.exports = config
+// added by @hellocoop/quickstart --nextjs on 2025-06-28T14:31:25.193Z
+config.client_id = 'app_zdoFnyaLScxNmkAaJK2d4wPa_xtF'
